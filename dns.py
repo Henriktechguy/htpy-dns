92814b01d2e5bf48785a978d4ef4fc96c43d5c73
Henrik Techguy <htechguy99@gmail.com>
2020-07-08 20:20:45 +0000

    Initial commit

---
+++ dns.py
@@ -0,0 +1,13 @@
+print("loading libraries")
+import os
+import sys
+from subprocess import check_output
+import dns.resolver
+print("import completed")
+#import applicable libraries
+def main(resolvserv, answer):
+
+
+#start main prog
+print("Henrik Techguy's PyDNS")
+url = input("Please Enter Web Address: ")
