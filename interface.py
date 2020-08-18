240ec074437c60e18d348e55313642130573782b
Henrik Techguy <htechguy99@gmail.com>
2020-08-18 01:12:06 +0000

    separating interface and script

---
+++ interface.py
@@ -0,0 +1,23 @@
+print("loading modules")
+import platform
+import os
+import time
+time.sleep(1) #delay's program for one second
+#detecting os properties
+platprop = os.uname()
+
+print("module load and infoload complete")
+#htpy logo
+import htpylogo
+#dns logo - aaag generator blocks font
+print(" _____  _   _  _____ ")
+print("|  __ \| \ | |/ ____|")
+print("| |  | |  \| | (___  ")
+print("| |  | | . ` |\___ \ ")
+print("| |__| | |\  |____) |")
+print("|_____/|_| \_|_____/ ")
+
+print("HTPY DNS Tool")
+import platprop
+print("Running on " + platprop.architecture + ("bit ") + platprop.baseos + ("     Version ") + platprop.osrelease)
+print("LAN Hostname: " + platprop.netname + "")
